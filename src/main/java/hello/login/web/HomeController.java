e3f0cdf3a2ac921d50b535fdb336b1ab9dbe3ebd
DaeHo Kim <zzangisdaeho@naver.com>
2022-02-22 01:41:39 +0900

    login by Cookie(security low)

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,38 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Objects;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping
+    public String homeLogin(@CookieValue(value = "memberId", required = false) Long memberId, Model model){
+        if(Objects.isNull(memberId)) return "home";
+        else{
+            Member loginMember = memberRepository.findById(memberId);
+            if(Objects.isNull(loginMember)) return "home";
+            else{
+                model.addAttribute("member", loginMember);
+                return "loginHome";
+            }
+        }
     }
 }
